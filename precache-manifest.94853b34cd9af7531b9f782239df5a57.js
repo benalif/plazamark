253b4a4a46513d8d9c006d84c7e98f7e3081812e
benalif <farid.benali89@gmail.com>
2020-12-02 12:34:36 +0100

    Updates

---
+++ precache-manifest.94853b34cd9af7531b9f782239df5a57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8412182d056697c7c97264e8e3d05e3",
+    "url": "/plazamark/index.html"
+  },
+  {
+    "revision": "14c7faf7b164f936257b",
+    "url": "/plazamark/static/css/main.40c3aabc.chunk.css"
+  },
+  {
+    "revision": "5a7fc3df9f2ea0d195ab",
+    "url": "/plazamark/static/js/2.46ce8970.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/plazamark/static/js/2.46ce8970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c7faf7b164f936257b",
+    "url": "/plazamark/static/js/main.87b977e6.chunk.js"
+  },
+  {
+    "revision": "5a98d212544bb60db93c",
+    "url": "/plazamark/static/js/runtime-main.3db92c62.js"
+  }
+]);
